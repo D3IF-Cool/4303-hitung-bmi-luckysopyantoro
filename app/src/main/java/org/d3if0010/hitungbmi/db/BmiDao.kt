8b7feae75c4b328e60cc7f4b855e48081161e6ab
luckysopyantoro <lucky.pratamas5@gmail.com>
2021-04-25 00:32:50 +0700

    3.3.Membuat database dengan Room
    (cherry picked from commit 2d8da1e05c116957cfe38047a3a581d1aab32b8f)

--- app/src/main/java/org/d3if0010/hitungbmi/db/BmiDao.kt
+++ app/src/main/java/org/d3if0010/hitungbmi/db/BmiDao.kt
@@ -9,8 +9,13 @@ import androidx.room.Query
 interface BmiDao {
     @Insert
     fun insert(bmi: BmiEntity)
+<<<<<<< HEAD
     @Query("SELECT * FROM bmi ORDER BY id DESC")
     fun getLastBmi(): LiveData<List<BmiEntity>>
     @Query("DELETE FROM bmi")
     fun clearData()
+=======
+    @Query("SELECT * FROM bmi ORDER BY id DESC LIMIT 1")
+    fun getLastBmi(): LiveData<BmiEntity?>
+>>>>>>> 2d8da1e... 3.3.Membuat database dengan Room
 }
